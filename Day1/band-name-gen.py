b1d5942206146fe87fbc8151c8cda62a88a07fd1
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-10 20:53:31 +0545

    Create band-name-gen.py
    In the first day i learned about basics of python programming language like printing,commenting,debugging,string manipulation and variables. I knew about this things before in fact i have wrote some scripts before but i wanted to do it properly from scratch.

---
+++ Day1/band-name-gen.py
@@ -0,0 +1,5 @@
+#band name generator 
+print("This is a band name generator.")
+city_name = input("What is the name of your city?\n")
+pet_name = input("What is your pet name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
