6c58394929892d044f43057c8154851241f8229c
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-11 20:33:17 +0545

    Create DataTypeChallenge.py

---
+++ Day2/DataTypeChallenge.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+first_digit_number = int(two_digit_number[0])
+second_digit_number = int(two_digit_number[1])
+sum = first_digit_number + second_digit_number
+print(first_digit_number,"+",second_digit_number,"=",sum)
