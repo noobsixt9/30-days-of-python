d320cd714edfa714e5cbed9eb0bb3b6a50768c4a
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-11 21:02:00 +0545

    Create BMI.py
    program that calculates the Body Mass Index (BMI) from a user's weight and height.
    
    The BMI is a measure of some's weight taking into account their height. e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
    
    The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
    BMI = weight(kg) / (height * height)(m*m)

---
+++ Day2/BMI.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+height_1 = float(height)
+weight_1 = int(weight)
+bmi = int(weight_1/(height_1**2))
+print(bmi)
