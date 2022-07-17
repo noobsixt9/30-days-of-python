749705f51e9576bd6e6ccec91b9495b70bd62722
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-17 21:05:45 +0545

    Update BankerRoulette.py

--- Day4/BankerRoulette.py
+++ Day4/BankerRoulette.py
@@ -1,6 +1,4 @@
 import random
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
 
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
