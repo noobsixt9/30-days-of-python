f39dbee1a1252e27a1b2ad95fcf40b155186aba3
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-17 21:08:23 +0545

    Update BankerRoulette.py

--- Day4/BankerRoulette.py
+++ Day4/BankerRoulette.py
@@ -1,10 +1,10 @@
 import random
-
 # Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
+names_string = input("Give me everybody's names, separated by a comma: ")
+names = names_string.split(",")
 
 name_num = len(names)
 random_int = random.randint(0, name_num-1)
 name_to_pay = names[random_int]
 print(name_to_pay,"is going to buy the meal today!")
+
