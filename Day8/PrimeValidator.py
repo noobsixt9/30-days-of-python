24d2143f411639b98bb2043cd0cb717143cdaef5
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-18 08:04:04 +0545

    Create PrimeValidator.py

---
+++ Day8/PrimeValidator.py
@@ -0,0 +1,12 @@
+def prime(num):
+  is_prime = True
+  for i in range(2,num):
+    if num % i == 0:
+      is_prime = False
+  if is_prime:
+    print("It's a prime number.")
+  else:
+    print("It's not a prime number.")
+
+n = int(input("Enter number to check: "))
+prime(num=n)
