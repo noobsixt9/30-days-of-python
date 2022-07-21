bc5874599a0683a8242c2977c62b98b54c7c0eb3
noobsixt9 <86828330+noobsixt9@users.noreply.github.com>
2022-07-21 11:15:30 +0545

    Update main.py

--- Day11/BlackJack/main.py
+++ Day11/BlackJack/main.py
@@ -1,78 +1,72 @@
 ############### Blackjack Project #####################
 import random
-from art import logo
 from replit import clear
-
-
+from art import logo
 
 def deal_card():
-  """ returns one random card from list """
   cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-  card = random.choice(cards)
-  return card
-  
+  return random.choice(cards)
+
 def calculate_score(card_list):
-  """calculate the total sum"""
-  if sum(card_list) > 21 and len(card_list) == 2:
+  if sum(card_list) == 21 and len(card_list) == 2:
     return 0
   if 11 in card_list and sum(card_list) > 21:
     card_list.remove(11)
-    card_list.append(1)  
+    card_list.append(1)
   return sum(card_list)
 
 def compare(user_score, computer_score):
-  """Compares user score and computer score"""
-  if user_score == 21 and computer_score > 21:
-    return "You lose. You went over  😤"
+  if user_score > 21 and computer_score > 21:
+    return "You went over. You lose."
     
   if user_score == computer_score:
-    return "It's a draw 🙃"
+    return "Draw"
   elif computer_score == 0:
-    return "You lose. Computer got a blackjack 😱"
+    return "Computer got a blackjack. You lose."
   elif user_score == 0:
-    return "You win. You got a blacjack 😎"
-  elif user_score > 21:
-    return "You lose. You went over 😭 "
+    return "You got a blackjack. You win."
   elif computer_score > 21:
-    return "You win. Computer went over 😁"
+    return "Computer went over. You win."
+  elif user_score > 21:
+    return "You went over. You lose"
   elif user_score > computer_score:
-    return "You win 😃"
+    return "You win."
   else:
-    return "You lose  😤"
-def play_game(): 
-  
+    return "You lose."
+def play_game():
   print(logo)
-  
   user_cards = []
   computer_cards = []
-  is_game_over = False
+  is_game_end = False
   
-  for _ in range(2):
+  for i in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
-  
-  while not is_game_over:
-    score_check_user = calculate_score(user_cards)
-    score_check_computer = calculate_score(computer_cards)
+
+  while not is_game_end:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
     print(f"\tYour cards: {user_cards}, current score: {calculate_score(user_cards)}")
     print(f"\tComputer's first card: {computer_cards[0]}")
     
-    if score_check_computer == 0 or score_check_user == 0 or score_check_user > 21:
-      is_game_over = True
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+      is_game_end = True
     else:
-      opt = input("Type 'y' to add another card and 'n' to pass: ")
+      
+      opt = input("Do you want to add another card or pass (type 'y' for yes and 'n' for no): ")
       if opt == "y":
         user_cards.append(deal_card())
       else:
-        is_game_over = True   
+        is_game_end = True
   
-  while score_check_computer != 0 and score_check_computer < 17:
+  while computer_score != 0 and computer_score < 17:
     computer_cards.append(deal_card())
-    score_check_computer = calculate_score(computer_cards)
-  print(f"\tYour final hand: {user_cards}, final score: {score_check_user}")
-  print(f"\tComputer's final hand: {computer_cards}, final score: {score_check_computer}")
-    
-  print(f"{compare(score_check_user, score_check_computer)}")
-while input("\nDo you want to play a game of BlackJack('y' for yes and 'n' for no): ") == "y":
-    clear()
-    play_game()
+    computer_score = calculate_score(computer_cards)
+  print(f"\tYour final cards: {user_cards}, final score: {user_score}")
+  print(f"\tComputer's final cards: {computer_cards}, final score: {computer_score}")
+  
+  print(compare(user_score, computer_score))
+
+while input("Do you want to play a game of BlackJack?(Type 'y' for yes and 'n' for no): ") == "y":
+  clear()
+  play_game()
